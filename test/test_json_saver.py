6971ee0a50b310b437b40033c41123b1d1ba7135
Gridnev Nikita <gridnevnikit4@gmail.com>
2025-07-10 19:13:43 +0300

    fixed project

--- test/test_json_saver.py
+++ test/test_json_saver.py
@@ -4,13 +4,11 @@ from src.json_saver import JSONFileHandler
 
 @pytest.fixture
 def json_handler(tmp_path):
-    # Создаём временный файл для тестов
     file = tmp_path / "test_vacancies.json"
     return JSONFileHandler(filename=str(file))
 
 
 def test_load_empty_file(json_handler):
-    # При отсутствии файла возвращается пустой список
     assert json_handler.load() == []
 
 
@@ -37,7 +35,7 @@ def test_no_duplicate_vacancies(json_handler):
     json_handler.add_vacancy(vacancy)
     json_handler.add_vacancy(vacancy)
     data = json_handler.load()
-    assert len(data) == 1  # Дубликаты не добавляются
+    assert len(data) == 1
 
 
 def test_delete_vacancy(json_handler):
